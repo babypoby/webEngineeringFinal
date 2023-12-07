9a9006e680b9c66f1ae08f054fbc7da303454eb1
Linus Meierhoefer <linusmeierhoefer@protonmail.com>
2023-12-07 20:53:52 +0100

    added basic backend

--- backend-project/src/dummy_server/router/app.py
+++ backend-project/src/dummy_server/router/app.py
@@ -1,54 +1,18 @@
-import argparse
-import os
+from flask import Flask, jsonify
+import json
 
-from flask import Flask
-from flask_cors import CORS
+app = Flask(__name__)
 
-from dummy_server.router.routes import add_routes
+@app.route('/geojson')
+def get_geojson():
+    try:
+        with open('/Users/linusmeiehofer/Documents/Code/FundamentalsWebEngineering/lmeierhoefer_project_flask/backend-project/src/dummy_server/resources/accessibility_1.geojson', 'r') as file:
+            geojson_data = json.load(file)
+            return jsonify(geojson_data)
+    except FileNotFoundError:
+        return jsonify(error="GeoJSON file not found."), 404
+    except json.JSONDecodeError:
+        return jsonify(error="Error decoding GeoJSON file."), 500
 
-
-def create_app():
-    app = Flask(__name__)  # static_url_path, static_folder, template_folder...
-    CORS(app, resources={r"/*": {"origins": "*"}})
-    add_routes(app)
-
-    @app.route('/version')
-    def version():
-        return f"Job ID: {os.environ['JOB_ID']}\nCommit ID: {os.environ['COMMIT_ID']}"
-
-    @app.route('/dargons')
-    def dargons():
-        return f"There be dragons here!"
-
-    return app
-
-
-def start_server():
-    parser = argparse.ArgumentParser()
-
-    # API flag
-    parser.add_argument(
-        "--host",
-        default="127.0.0.1",
-        help="The host to run the server",
-    )
-    parser.add_argument(
-        "--port",
-        default=8000,
-        help="The port to run the server",
-    )
-    parser.add_argument(
-        "--debug",
-        action="store_true",
-        help="Run Flask in debug mode",
-    )
-
-    args = parser.parse_args()
-
-    server_app = create_app()
-
-    server_app.run(debug=args.debug, host=args.host, port=args.port)
-
-
-if __name__ == "__main__":
-    start_server()
+if __name__ == '__main__':
+    app.run(debug=True)

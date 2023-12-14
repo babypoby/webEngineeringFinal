23f2afee69ed8a46d2ff4be32e6c5a8d608ddbb7
Tae <babypoby@gmail.com>
2023-12-14 23:09:33 +0100

    It works! :)

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup, LayersControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup } from 'react-leaflet';
 import L, { Layer } from 'leaflet';
 
 
@@ -18,26 +18,34 @@ const App = () => {
       .then((response) => response.json())
       .then((geojson) => {
         const formattedCoordinates = geojson.features.map(x => ({
-          coordinates: x.geometry.coordinates, 
+          coordinates: x.geometry.coordinates,
           properties: x.properties
         }));
+        console.log("trainstations", formattedCoordinates)
         setTrainCoordinates(formattedCoordinates);
       })
       .catch(error => console.error('Error fetching data: ', error));
 
-      fetch("http://localhost:8000/api/data/parkingspaces")
+    fetch("http://localhost:8000/api/data/parkingspaces")
       .then((response) => response.json())
       .then((geojson) => {
-        const formattedCoordinates = geojson.features.map(x => ({
-          coordinates: x.geometry.coordinates, 
-          properties: x.properties
+        const groups = groupByAddress(geojson.features);
+        const formattedCoordinates = Object.values(groups).map(x => ({
+          coordinates: ToReversed(x[0].geometry.coordinates),
+          properties: x[0].properties,
+          count: x.length
         }));
+        console.log("parkingspaces", formattedCoordinates)
         setParkingCoordinates(formattedCoordinates);
       })
       .catch(error => console.error('Error fetching data: ', error));
 
   }, []);
- 
+
+  useEffect(() => {
+    console.log("Parking Coordinates:", parkingcoordinates);
+  }, [parkingcoordinates]);
+
 
   function groupByAddress(features: any[]) {
     const groups: { [key: string]: any[] } = {};
@@ -52,58 +60,65 @@ const App = () => {
   }
 
 
+  function ToReversed<T>(inp: T[]): T[] {
+    const copy = [...inp]
+    copy.reverse();
+    return copy;
+  }
 
-  return( 
+  return (
     <div>
       <MapContainer center={[47.36667, 8.55]} zoom={13} scrollWheelZoom={false} className='mapContainer'>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <LayersControl position = "topright">
-        <LayersControl.Overlay name = "Trainstations">
-        {traincoordinates.map((item, index) => (
-          <Marker key={index} position={item.coordinates.reverse()} icon={L.icon({iconUrl: "/icons/icon-blue.png",
-              iconSize: [100, 100],
-              iconAnchor: [50, 50],
-              popupAnchor: [0, -20],})}>
-            <Popup>
-              {
-                JSON
-                .stringify(item.properties, null, "\t")
-                .replaceAll(
-                    "],\n\t\"", 
-                    "],\n\n\t\""
-                )
-              }
-            </Popup>
-          </Marker>
-        ))}
-        </LayersControl.Overlay>
-        <LayersControl.Overlay name = "Parkingspaces">
-        {parkingcoordinates.map((item, index) => (
-          <Marker key={index} position={item.coordinates.reverse()} icon={L.icon({iconUrl: "/icons/icon-orange.png",
-              iconSize: [100, 100],
-              iconAnchor: [50, 50],
-              popupAnchor: [0, -20],})}>
-            <Popup>
-              {
-                JSON
-                .stringify(item.properties, null, "\t")
-                .replaceAll(
-                    "],\n\t\"", 
-                    "],\n\n\t\""
-                )
-              }
-            </Popup>
-          </Marker>
-        ))}
-        </LayersControl.Overlay>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LayersControl position="topright">
+          <LayersControl.Overlay name="Trainstations">
+            <LayerGroup>
+              {traincoordinates.map((item, index) => (
+                <Marker key={index} position={item.coordinates} icon={L.icon({
+                  iconUrl: "/icons/icon-blue.png",
+                  iconSize: [100, 100],
+                  iconAnchor: [50, 50],
+                  popupAnchor: [0, -20],
+                })}>
+                  <Popup>
+                    {
+                      JSON
+                        .stringify(item.properties, null, "\t")
+                        .replaceAll(
+                          "],\n\t\"",
+                          "],\n\n\t\""
+                        )
+                    }
+                  </Popup>
+                </Marker>
+              ))}
+            </LayerGroup>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name="Parkingspaces">
+            <LayerGroup>
+              {parkingcoordinates.map((item, index) => (
+                <Marker key={index} position={item.coordinates.reverse()} icon={L.icon({
+                  iconUrl: "/icons/icon-orange.png",
+                  iconSize: [100, 100],
+                  iconAnchor: [50, 50],
+                  popupAnchor: [0, -20],
+                })}>
+                  <Popup>
+                    Address: {item.properties.adresse} <br />
+                    Type: {item.properties.art} <br />
+                    Number of available parking spaces: {item.count}
+                    Fee Required: {item.properties.gebpflicht === '1' ? 'Yes' : 'No'} <br />'
+                  </Popup>
+                </Marker>
+              ))}
+            </LayerGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
 
-      
-      </LayersControl>
-      
-    </MapContainer>
+      </MapContainer>
 
     </div>
   );

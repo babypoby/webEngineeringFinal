1cfbdea892f66efb7f021ece65e56bec094a2a75
Linus Meierhoefer <linusmeierhoefer@protonmail.com>
2023-12-19 10:31:50 +0100

    reworked statisticspanel

--- react-frontend/src/StatisticsPanel.tsx
+++ react-frontend/src/StatisticsPanel.tsx
@@ -1,20 +1,26 @@
 import './StatisticsPanel.css';
 import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
 import type { PointLayer} from './types/statistics';
-import Graph from './Graph';
+import Barplot from "./Barplot";
+import { PieChart } from 'recharts';
+
 
 const StatisticsPanel = ({ statistics }: { statistics: PointLayer[] }) => {
     
+    console.log(statistics)
+
     return (
-        <div className='statistics-panel'>
-            <h2>Statistics</h2>
+        <div className="w-full md:w-1/4 p-4 bg-white">
+            <div className='text-center'>
+                <h2 className='text-lg font-semibold border-b-2 border-gray-300 py-2'>Statistics</h2>
+            </div>
             {
                 statistics.map((layer, index) => (
-                    <div key={index}>
-                        <h3>{layer.name}</h3>
+                    <div key={index} className='text-center'>
+                        <h3 className="text-sm text-gray-600">{layer.name}</h3>
                         <p>{layer.coordinates.length}</p>
-                        <Graph layer = {layer}/>
+                        {(layer.name === "Trainstations") ? <Barplot data={layer.coordinates}/> : 
+                        (layer.name == "Parkingspaces") ? <PieChart data={layer.coordinates}/> : <p></p>}  
                     </div>
                 ))
             }
